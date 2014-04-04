4a093f73331d3d540901c7e238040a95aa152b11
Brooke Bryan <brooke@bajb.net>
2014-04-04 16:02:59 +0100

    Dispatch enabled and configured by default

--- public/index.php
+++ public/index.php
@@ -7,9 +7,30 @@ require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 //Create an instance of cubex, with the web root defined
 $app = new \Cubex\Cubex(__DIR__);
+$app->boot();
 
-//If you wish to use stackphp, uncomment the line below and require stackphp
-//$stack = (new \Stack\Builder())->push(/* middleware */)->resolve($app);
+//Create and configure a new dispatcher
+$dispatcher = new \Packaged\Dispatch\Dispatch(
+  $app,
+  $app->getConfiguration()->getSection('dispatch')
+);
+
+//Set the correct working directory for dispatcher
+$dispatcher->setBaseDirectory(dirname(__DIR__));
+
+//Load in the cache of file hashes to improve performance of dispatched assets
+$fileHash = 'conf/dispatch.filehash.ini';
+if(file_exists($fileHash))
+{
+  $hashTable = parse_ini_file($fileHash, false);
+  if(!empty($hashTable))
+  {
+    $dispatcher->setFileHashTable($hashTable);
+  }
+}
+
+//Inject dispatch to handle assets
+$app = (new \Stack\Builder())->push([$dispatcher, 'prepare'])->resolve($app);
 
 //Create a request object
 $request = \Cubex\Http\Request::createFromGlobals();
